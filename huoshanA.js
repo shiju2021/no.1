4bb85fbad0aea8a3d605f392c7ae67eff7c321f1
2ge1 <76772214+2ge1@users.noreply.github.com>
2021-02-03 16:30:30 +0800

    Update huoshanA.js

--- huoshanA.js
+++ huoshanA.js
@@ -1,3 +1,5 @@
+/*197delay4000
+*/
 const HS = '抖音火山版'
 const $ = Env(HS)
 const notify = $.isNode() ?require('./sendNotify') : '';
@@ -194,7 +196,7 @@ for(let i = 0;i <= 4;i++){
    item_id_inv = item_id[i]
    $.log(item_id_inv)
    let x = Math.random()
-   let delay = x > 0.5? x*60000 : (x+0.5)*30000
+   let delay = x > 0.5? x*60000 : (x+0.5)*40000
    console.log('⏰本次延迟'+Math.round(delay/1000)+'秒')
    await sleep(delay)
    await play_video()
